34b4b535d7d86193f5d3b6c757804c79eb8054d8
Rahul Kakarlapudi <skakar3@uic.edu>
2021-04-11 05:00:09 +0000

    added code for GitHub user site activity

---
+++ projects.js
@@ -0,0 +1,42 @@
+let projects = [];
+
+projects.push({
+  projectName: "Space Explorers",
+  projectDescription: "Space Explorers is an installable PWA(Progressive Web Application). The user will attempt to avoid collision with harm objects while trying to gain points and lives by collecting coins and other beneficial objects. The project utilizes features of the HTML canvas element to support the animation of the game.",
+  technicalData: "Languages: HTML, CSS, Javascript",
+  linkToRepo: "https://github.com/skakar3/IT202-Spring2021-project2",
+  linkToFunctioningPage: "https://skakar3.github.io/IT202-Spring2021-project2/",
+  image: "images/project1.png",
+
+  display: true
+});
+
+projects.push({
+  projectName: "Chicago Land Inventory Navigator",
+  projectDescription: "This Application allows the user to search for City of Chicago City-Owned Land Inventory data. It includes search parameters that allow the users to narrow down their search. The application displays information of relevant plots and also maps them on an interactive Google map.",
+  technicalData: "Languages: HTML, CSS, Javascript",
+  linkToRepo: "https://github.com/skakar3/IT202-Spring2021-project1",
+  linkToFunctioningPage: "https://skakar3.github.io/IT202-Spring2021-project1/",
+  image: "images/project2.png",
+  display: true
+});
+
+projects.push({
+  projectName: "Face Mask Generator",
+  projectDescription: "This Application allows the user to choose from multiple face masks and then capture their image. The application then displays the user's image after applying the mask of their choice to it.",
+  technicalData: "Languages: HTML, CSS, Javascript",
+  linkToRepo: "https://github.com/skakar3/IT202-Spring2021-activities",
+  linkToFunctioningPage: "https://skakar3.github.io/IT202-Spring2021-activities/lab9.html",
+  image: "images/mask.png",
+  display: true
+});
+
+projects.push({
+  projectName: "Face Mask Generator",
+  projectDescription: "This Application displays the user's geolocation trail on an interactive map. The application utilizes the Google Maps API and the user's geolocation to determine and display the user's geolocation trail.",
+  technicalData: "Languages: HTML, CSS, Javascript",
+  linkToRepo: "https://github.com/skakar3/IT202-Spring2021-activities",
+  linkToFunctioningPage: "https://skakar3.github.io/IT202-Spring2021-activities/locationAwareMap.html",
+  image: "images/geoLocation.png",
+  display: false
+});
